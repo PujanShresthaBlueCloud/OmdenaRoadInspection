d9927bbd29fd68bb43ec74b6bfc4e31ad5bdd533
Abirna Shrestha <pujanshrestha@192-168-1-102.tpgi.com.au>
2023-06-16 14:41:45 +1000

    added js

--- app.py
+++ app.py
@@ -72,8 +72,9 @@ def main():
         st.markdown(f"<style>{source_style.read()}</style>", 
              unsafe_allow_html = True)
     with open("main.js", "r") as source_js:
-        st.markdown(f"<script>{source_js.read()}</script>",
-            unafe_allow_htmt = True)
+        js_code = source_js.read()
+    st.markdown(js_code, unsafe_allow_html = True)
+
     st.image("logo1.png")
     st.title("AI Road Inspection System")        
     Header = st.container()
