bee3102179c7e06aeb08d8d5326fecf09feb3ef9
Abirna Shrestha <pujanshrestha@192-168-1-102.tpgi.com.au>
2023-06-16 13:51:59 +1000

    remove streamlit logo and hambuger menu

--- app.py
+++ app.py
@@ -214,7 +214,14 @@ def main():
         to improved road safety and optimized maintenance operations. 
 
         """
-        st.markdown(conclusion, unsafe_allow_html=True)    
+        st.markdown(conclusion, unsafe_allow_html=True)
+        hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+        st.markdown(hide_streamlit_style, unsafe_allow_html=True)   
 if __name__ == '__main__':
     try:
         main()
