06fd27c83bd1d2a564f3c87f95d87a88e161a4b6
Abirna Shrestha <pujanshrestha@192-168-1-102.tpgi.com.au>
2023-06-16 14:53:25 +1000

    remove footer text

--- main.js
+++
@@ -1,4 +0,0 @@
-const elements = window.parent.document.getElementsByTagName('footer');
-// elements[0].innerHTML = "";
-alert("hello");
-// elements[0].innerHTML = "Mexico x Omdena VIT Bhopal Local Chapter " + new Date().getFullYear();
