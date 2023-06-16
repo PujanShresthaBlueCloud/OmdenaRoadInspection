6b4cf24a026bf7860921a2cc9368324f74a95ae9
Abirna Shrestha <pujanshrestha@192-168-1-102.tpgi.com.au>
2023-06-16 14:44:22 +1000

    added js

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
 const elements = window.parent.document.getElementsByTagName('footer');
+elements[0].innerHTML = "";
+alert("hello");
 elements[0].innerHTML = "Mexico x Omdena VIT Bhopal Local Chapter " + new Date().getFullYear();
