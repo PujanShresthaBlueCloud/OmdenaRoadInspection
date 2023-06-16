af23174f07254d8613859813ce5f317495e61f77
Abirna Shrestha <pujanshrestha@192-168-1-102.tpgi.com.au>
2023-06-16 13:47:52 +1000

    edit text

--- app.py
+++ app.py
@@ -95,24 +95,12 @@ def main():
     if selected == "Project Information":
         st.subheader("Problem Statement")
         problem_statement = """
-        Current practices of performing road inspections are time-consuming and labour-intensive. Road surfaces degrade on a 
-        daily basis as a result of the heavy traffic on them.This will not only impact the driver’s comfort but will also
-        impact economic efficiency. To maintain roads as efficiently as possible, municipalities perform regular
-        inspections. The aim of the project is to use machine learning to study and analyze different types of road defects
-        and to automatically detect any road abnormalities.
+        Current practices of performing road inspections are time-consuming and labour-intensive. Road surfaces degrade on a daily basis as a result of the heavy traffic on them.This will not only impact the driver’s comfort but will also impact economic efficiency. 
         """
         st.write(problem_statement)
         with st.expander("read more.."):
-            st.write(""" 
-            The goal of this project is to design, build and test an inspection system for detecting road abnormalities, defects, and damages
-            using machine learning. The proposed system aims to improve the efficiency of road inspections and reduce
-            the time and labor required for the process. The system will be equipped with a camera to capture video streams
-            from different roads, and the data will be analyzed using the Matlab machine learning toolbox to train and test the network.
-            The output of the system will be recommended actions for the municipality to fix/correct any identified road defects. 
-            The approach will involve three main tasks: data acquisition, data training/testing, and dashboard building and testing. 
-            Ultimately, the proposed system will help to maintain roads more efficiently, enhance driver comfort, and improve economic 
-            efficiency. Additionally, the system will provide insights into the causes of road abnormalities in Indian roads, 
-            including pitfalls, sinks, flooding, and traffic congestion due to insufficient lanes in cities and towns.
+            st.write("""
+            To maintain roads as efficiently as possible, municipalities perform regular inspections. The aim of the project is to use machine learning to study and analyze different types of road defects and automatically detect any road abnormalities. The goal of this project is to design, build and test an inspection system for detecting road abnormalities, defects, and damages using machine learning. The proposed system aims to improve the efficiency of road inspections and reduce the time and labor required for the process. The system will be equipped with a camera to capture video streams from different roads, and the data will be analyzed using the Matlab machine learning toolbox to train and test the network. The output of the system will be recommended actions for the municipality to fix/correct any identified road defects. The approach will involve three main tasks: data acquisition, data training/testing, and dashboard building and testing. Ultimately, the proposed system will help to maintain roads more efficiently, enhance driver comfort, and improve economic efficiency. Additionally, the system will provide insights into the causes of road abnormalities in Indian roads, including pitfalls, sinks, flooding, and traffic congestion due to insufficient lanes in cities and towns.
             """)
         
         st.subheader("Our Solution")
